6184cb3e31d2ed0142ffffccde7b69e63f8c65f3
kimyw97 <duddnrdpqj@gmail.com>
2025-05-09 10:54:27 +0900

    cg min 2

--- cow/raspberrypi/ros2_ws/src/cmdvel_to_serial_cpp/src/cmdvel_to_serial.cpp
+++ cow/raspberrypi/ros2_ws/src/cmdvel_to_serial_cpp/src/cmdvel_to_serial.cpp
@@ -89,8 +89,8 @@ private:
         int r = static_cast<int>(right_speed * 100);
     
         // 최소 PWM 보정 (0은 그대로, 1~49는 ±50으로 클램핑)
-        if (std::abs(l) > 0 && std::abs(l) < 70) l = (l > 0) ? 70 : -70;
-        if (std::abs(r) > 0 && std::abs(r) < 70) r = (r > 0) ? 70 : -70;
+        if (std::abs(l) > 0 && std::abs(l) < 100) l = (l > 0) ? 100 : -100;
+        if (std::abs(r) > 0 && std::abs(r) < 100) r = (r > 0) ? 100 : -100;
     
         std::stringstream ss;
         ss << "L" << l << "R" << r << "\n";
