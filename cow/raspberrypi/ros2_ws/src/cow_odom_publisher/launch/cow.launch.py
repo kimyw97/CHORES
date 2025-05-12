15879c1da87d07edb2df1e93e05f82d546915c7f
kimyw97 <duddnrdpqj@gmail.com>
2025-05-12 11:08:38 +0900

    fix -3

--- cow/raspberrypi/ros2_ws/src/cow_odom_publisher/launch/cow.launch.py
+++ cow/raspberrypi/ros2_ws/src/cow_odom_publisher/launch/cow.launch.py
@@ -2,7 +2,7 @@ from launch import LaunchDescription
 from launch_ros.actions import Node
 from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.substitutions import ThisLaunchFileDir, FindExecutable
+from launch_ros.substitutions import FindPackageShare
 import os
 
 def generate_launch_description():
