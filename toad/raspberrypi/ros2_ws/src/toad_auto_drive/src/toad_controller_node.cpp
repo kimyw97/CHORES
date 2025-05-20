28e66eaff117d478010b34fbecb71bb29b31ba5e
kwy98ST <kwy98ST@github.com>
2025-05-20 12:56:16 +0900

    trash-toad

--- toad/raspberrypi/ros2_ws/src/toad_auto_drive/src/toad_controller_node.cpp
+++ toad/raspberrypi/ros2_ws/src/toad_auto_drive/src/toad_controller_node.cpp
@@ -4,7 +4,7 @@
 #include <iostream>
 #include <string>
 #include "toad_auto_drive/msg/toad_drive_msg.hpp"
-#include "toad_auto_drive/msg/trash_info.hpp"
+#include "trash_camera/msg/trash_info.hpp"
 #include "geometry_msgs/msg/point.hpp"
 
 using std::placeholders::_1;
@@ -12,7 +12,7 @@ using std::placeholders::_1;
 class ToadControllerNode : public rclcpp::Node{
     public :
     ToadControllerNode() : Node("toad_controller_node"){
-        cap_subscription_ = this->create_subscription<toad_auto_drive::msg::TrashInfo>(
+        cap_subscription_ = this->create_subscription<trash_camera::msg::TrashInfo>(
             "trash_info", 10,
             std::bind(&ToadControllerNode::trashDetectCallback, this, _1)
         );
@@ -42,21 +42,14 @@ class ToadControllerNode : public rclcpp::Node{
     float distance_cm;
     geometry_msgs::msg::Point center;
 
-    rclcpp::Subscription<toad_auto_drive::msg::TrashInfo>::SharedPtr cap_subscription_;
+    rclcpp::Subscription<trash_camera::msg::TrashInfo>::SharedPtr cap_subscription_;
     rclcpp::Subscription<toad_auto_drive::msg::ToadDriveMsg>::SharedPtr drive_subscription_;
 
 
-    void trashDetectCallback(const toad_auto_drive::msg::TrashInfo::SharedPtr msg){
+    void trashDetectCallback(const trash_camera::msg::TrashInfo::SharedPtr msg){
         is_trash = msg->is_trash;
         distance_cm = msg->distance_cm;
 	center = msg->center;
-
-	 if(is_trash){
-                    
-                    RCLCPP_INFO(this->get_logger(), "%f cm", distance_cm);
-
-            }
-
 
         controller();
     }
@@ -69,7 +62,12 @@ class ToadControllerNode : public rclcpp::Node{
     }
     
     void controller(){
-	 
+	  if(is_trash){
+
+                    RCLCPP_INFO(this->get_logger(), "%f cm", distance_cm);
+
+            }
+/*
         if(is_trash){
             if(distance_cm > 30.0){
                 std::string message = "L" + std::to_string(30) + "R" + std::to_string(30) + "\n";
@@ -83,7 +81,7 @@ class ToadControllerNode : public rclcpp::Node{
         }else{
             std::string message = "L" + std::to_string(30) + "R" + std::to_string(30) + "\n";
             serial_.write(message);
-        }
+        } */
     }
 
 };
