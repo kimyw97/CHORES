85ba313af291a74a4b2fc1fac06fc0a9f5986382
kimyw97 <duddnrdpqj@gmail.com>
2025-06-04 15:22:22 +0900

    add acc to robot stataus publisher

--- cow/raspberrypi/ros2_ws/src/robot_monitoring/src/robot_monitor_node.cpp
+++ cow/raspberrypi/ros2_ws/src/robot_monitoring/src/robot_monitor_node.cpp
@@ -25,36 +25,39 @@ public:
 
 private:
   void readSerial() {
-    char ch;
-    while (serial_.get(ch)) {
-      if (ch == '\n') {
-        // 한 줄 완성
-        RCLCPP_INFO(this->get_logger(), "Raw line: '%s'", buffer_.c_str());
-  
-        std::regex regex("SPEED:L(-?\\d+),R(-?\\d+);TRASH:(\\d);EMERGENCY:(\\d);ENCODER:L(-?\\d+),R(-?\\d+)");
-        std::smatch match;
-  
-        if (std::regex_search(buffer_, match, regex)) {
-          auto msg = robot_monitoring::msg::RobotStatus();
-          msg.left_speed = std::stoi(match[1]);
-          msg.right_speed = std::stoi(match[2]);
-          msg.trash_full = std::stoi(match[3]) == 1;
-          msg.emergency = std::stoi(match[4]) == 1;
-          msg.left_encoder = std::stoi(match[5]);
-          msg.right_encoder = std::stoi(match[6]);
-  
-          status_pub_->publish(msg);
-          RCLCPP_INFO(this->get_logger(), "Published robot status.");
-        } else {
-          RCLCPP_WARN(this->get_logger(), "Regex match failed for line: '%s'", buffer_.c_str());
-        }
-  
-        buffer_.clear();  // 버퍼 초기화
+  char ch;
+  while (serial_.get(ch)) {
+    if (ch == '\n') {
+      RCLCPP_INFO(this->get_logger(), "Raw line: '%s'", buffer_.c_str());
+
+      std::regex regex("SPEED:L(-?\\d+),R(-?\\d+);TRASH:(\\d);EMERGENCY:(\\d);ENCODER:L(-?\\d+),R(-?\\d+);ACC:(-?\\d+),(-?\\d+),(-?\\d+)");
+      std::smatch match;
+
+      if (std::regex_search(buffer_, match, regex)) {
+        auto msg = robot_monitoring::msg::RobotStatus();
+        msg.left_speed = std::stoi(match[1]);
+        msg.right_speed = std::stoi(match[2]);
+        msg.trash_full = std::stoi(match[3]) == 1;
+        msg.emergency = std::stoi(match[4]) == 1;
+        msg.left_encoder = std::stoi(match[5]);
+        msg.right_encoder = std::stoi(match[6]);
+        msg.acc_x = std::stoi(match[7]);
+        msg.acc_y = std::stoi(match[8]);
+        msg.acc_z = std::stoi(match[9]);
+
+        status_pub_->publish(msg);
+        RCLCPP_INFO(this->get_logger(), "Published robot status with accelerometer.");
       } else {
-        buffer_ += ch;  // 계속 이어붙임
+        RCLCPP_WARN(this->get_logger(), "Regex match failed for line: '%s'", buffer_.c_str());
       }
+
+      buffer_.clear();
+    } else {
+      buffer_ += ch;
     }
   }
+}
+
 
   std::ifstream serial_;
   std::string buffer_;
