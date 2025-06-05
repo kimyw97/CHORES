f22d74dfe806f73eee2b0e3cf8fb3dea4cb5fa40
kwy98 <danny981027@gmail.com>
2025-06-05 17:08:04 +0900

    action test

--- toad/raspberrypi/ros2_ws/src/toad_auto_drive/src/toad_action_controller.cpp
+++ toad/raspberrypi/ros2_ws/src/toad_auto_drive/src/toad_action_controller.cpp
@@ -139,11 +139,10 @@ private:
     } else if (left_ir == 0 && right_ir == 1) {
       RCLCPP_INFO(this->get_logger(), "Turn Left");
       cmd = "L-30R30\n";
-    } /*else {
-      RCLCPP_INFO(this->get_logger(), "stop!!!!!");
-      cmd = "L0R0\n";
+    } else {
+      stop();
     }
-*/
+
     writeSerial(cmd);
   }
 
