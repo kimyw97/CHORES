0121ef16041722870ce4dd2026b64c7ce8d65b39
kwy98 <danny981027@gmail.com>
2025-06-05 18:18:11 +0900

    action test

--- toad/raspberrypi/ros2_ws/src/toad_auto_drive/src/toad_action_controller.cpp
+++ toad/raspberrypi/ros2_ws/src/toad_auto_drive/src/toad_action_controller.cpp
@@ -139,7 +139,7 @@ private:
     } else if (left_ir == 0 && right_ir == 1) {
       RCLCPP_INFO(this->get_logger(), "Turn Left");
       cmd = "L-30R30\n";
-    } else {
+    } else if(left_ir == 0 && right_ir == 0) {
       stop();
     }
 
