2559259c4523c59f777eedf8dcf9be2a27f937aa
kwy98 <danny981027@gmail.com>
2025-06-05 16:22:40 +0900

    action test

--- toad/raspberrypi/ros2_ws/src/toad_auto_drive/src/toad_action_controller.cpp
+++ toad/raspberrypi/ros2_ws/src/toad_auto_drive/src/toad_action_controller.cpp
@@ -91,7 +91,7 @@ private:
       feedback->status = "IR values: L=" + std::to_string(left_ir) + ", R=" + std::to_string(right_ir);
       goal_handle->publish_feedback(feedback);
 
-      rclcpp::sleep_for(std::chrono::milliseconds(200));
+      rclcpp::sleep_for(std::chrono::milliseconds(2000));
     }
 
     if (goal_handle->is_canceling()) {
