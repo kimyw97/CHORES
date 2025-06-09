46165d0a0c4e4bc62560849cdfa24df5e6e8f806
kimyw97 <duddnrdpqj@gmail.com>
2025-06-09 15:41:27 +0900

    fix covariances

--- cow/raspberrypi/ros2_ws/src/cow_odom_publisher/src/cow_odom_publisher.cpp
+++ cow/raspberrypi/ros2_ws/src/cow_odom_publisher/src/cow_odom_publisher.cpp
@@ -130,9 +130,9 @@ private:
     imu.angular_velocity_covariance[4] = 0.05;
     imu.angular_velocity_covariance[8] = 0.1;
 
-    imu.linear_acceleration_covariance[0] = 0.2;
-    imu.linear_acceleration_covariance[4] = 0.2;
-    imu.linear_acceleration_covariance[8] = 0.2;
+    imu.linear_acceleration_covariance[0] = 0;
+    imu.linear_acceleration_covariance[4] = 0;
+    imu.linear_acceleration_covariance[8] = 0;
 
     imu_pub_->publish(imu);
 
