63a22bbfca96d6c9f7555f0be506e4ecfb20a432
kimyw97 <duddnrdpqj@gmail.com>
2025-06-09 15:26:39 +0900

    fix

--- cow/raspberrypi/ros2_ws/src/cow_odom_publisher/src/cow_odom_publisher.cpp
+++ cow/raspberrypi/ros2_ws/src/cow_odom_publisher/src/cow_odom_publisher.cpp
@@ -62,7 +62,7 @@ private:
     if (std::abs(delta_left - delta_right) > 20) {
       d_theta = 0.0;
     }
-    const int ENCODER_DEADBAND = 4;
+    const int ENCODER_DEADBAND = 10;
     if (std::abs(delta_left) < ENCODER_DEADBAND) {
       delta_right = 0;
     }
