1094154a1c2e69f5aae737624d91c11bd5f608b5
kimyw97 <duddnrdpqj@gmail.com>
2025-07-01 11:06:10 +0900

    remove joint state pub

--- cow/raspberrypi/ros2_ws/src/cow_odom_publisher/src/cow_odom_publisher.cpp
+++ cow/raspberrypi/ros2_ws/src/cow_odom_publisher/src/cow_odom_publisher.cpp
@@ -94,14 +94,14 @@ private:
     odom.twist.covariance[35] = 0.2;
     odom_pub_->publish(odom);
 
-    double left_angle = left_ticks * (2.0 * M_PI / TICKS_PER_REV);
-    double right_angle = right_ticks * (2.0 * M_PI / TICKS_PER_REV);
-    sensor_msgs::msg::JointState joint_state;
-    joint_state.header.stamp = current_time;
-    joint_state.name = {"left_wheel_joint", "right_wheel_joint",
-                        "base_footprint_to_base_link", "base_link_to_base_scan"};
-    joint_state.position = {left_angle, right_angle, 0.0, 0.0};
-    joint_pub_->publish(joint_state);
+    // double left_angle = left_ticks * (2.0 * M_PI / TICKS_PER_REV);
+    // double right_angle = right_ticks * (2.0 * M_PI / TICKS_PER_REV);
+    // sensor_msgs::msg::JointState joint_state;
+    // joint_state.header.stamp = current_time;
+    // joint_state.name = {"left_wheel_joint", "right_wheel_joint",
+    //                     "base_footprint_to_base_link", "base_link_to_base_scan"};
+    // joint_state.position = {left_angle, right_angle, 0.0, 0.0};
+    // joint_pub_->publish(joint_state);
 
     sensor_msgs::msg::Imu imu;
     imu.header.stamp = current_time;
