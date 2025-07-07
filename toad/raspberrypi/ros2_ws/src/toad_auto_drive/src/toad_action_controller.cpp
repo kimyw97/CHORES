2f2d58a1c69889fa76b9d732befc548ffca9693c
kwy98 <danny981027@gmail.com>
2025-07-07 15:02:16 +0900

    좌우모터 속도 조절

--- toad/raspberrypi/ros2_ws/src/toad_auto_drive/src/toad_action_controller.cpp
+++ toad/raspberrypi/ros2_ws/src/toad_auto_drive/src/toad_action_controller.cpp
@@ -46,10 +46,11 @@ private:
     int s2 = goal->s2;
 
     std::string command;
-    std::string speed = "33";
-    if (s1 == 1 && s2 == 1) command = "L" + speed + "R" + speed;
-    else if (s1 == 1 && s2 == 0) command = "L-" + speed + "R" + speed;
-    else if (s1 == 0 && s2 == 1) command = "L"+ speed + "R-" + speed;
+    std::string L_speed = "33";
+    std::string R_speed = "35";
+    if (s1 == 1 && s2 == 1) command = "L" + L_speed + "R" + R_speed;
+    else if (s1 == 1 && s2 == 0) command = "L-" + L_speed + "R" + R_speed;
+    else if (s1 == 0 && s2 == 1) command = "L"+ L_speed + "R-" + R_speed;
     else command = "L0R0";
 
     feedback->status = "Processed IR values: s1=" + std::to_string(s1) + ", s2=" + std::to_string(s2);
