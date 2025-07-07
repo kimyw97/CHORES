8c2e874f95c7304b288509576b10fce2abe161e0
kwy98-2 <danny981027@gmail.com>
2025-07-07 16:49:06 +0900

    속도조정 9

--- toad/raspberrypi/ros2_ws/src/toad_auto_drive/src/toad_action_controller.cpp
+++ toad/raspberrypi/ros2_ws/src/toad_auto_drive/src/toad_action_controller.cpp
@@ -46,8 +46,8 @@ private:
     int s2 = goal->s2;
 
     std::string command;
-    std::string L_speed = "27";
-    std::string R_speed = "28";
+    std::string L_speed = "25";
+    std::string R_speed = "26";
     if (s1 == 1 && s2 == 1) command = "L" + L_speed + "R" + R_speed;
     else if (s1 == 1 && s2 == 0) command = "L-" + L_speed + "R" + R_speed;
     else if (s1 == 0 && s2 == 1) command = "L"+ L_speed + "R-" + R_speed;
